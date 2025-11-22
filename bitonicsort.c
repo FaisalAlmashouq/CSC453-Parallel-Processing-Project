b6a561257222ed794abf3ba39260ed6fc472f96d
faisal-almashouq <faisalmashouq@mawj.ai>
2025-11-22 03:14:55 +0300

    added data.txt odd sized data set. This step is everything before the actual parallelism of the project.

--- bitonicsort.c
+++ bitonicsort.c
@@ -4,15 +4,36 @@
 
 void load_data(int** data) {
     printf("Loading data");
+
+    FILE *file = fopen("data.txt", "r");
+    if (file == NULL) {
+        printf("Error opening file");
+        return;
+    }
+
+    int i = 0, num;
+    int* arr = malloc(100 * sizeof(int*));
+    while((fscanf(file, "%d", &num) == 1) && (i < 100)) {
+        arr[i++] = num;
+    }
+
+    
 }
 
 int** bitonic_sort(int** data) {
     printf("Sorting data using Bitonic Merge Sort Algorithm");
+
     return data;
 }
 int main() {
     int **unsorted;
+
     load_data(unsorted);
+    if (unsorted == NULL) {
+        printf("No data loaded");
+        return -1;
+    }
+
     if ((sizeof(unsorted)/sizeof(unsorted[0])) % 2 != 0) {
         printf("Array length must be even");
         return -1;
@@ -23,7 +44,6 @@ int main() {
     for (int i = 0; sorted[i] != NULL; i++) {
         printf("The new sorted data: %d ", *sorted[i]);
     }
-    
     
     return 0;
 }
