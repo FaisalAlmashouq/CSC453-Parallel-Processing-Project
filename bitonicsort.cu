7b9dde5193b478a2a2bc693af2c47592cc01c0fd
faisal-almashouq <faisalmashouq@mawj.ai>
2025-11-24 02:06:22 +0300

    Final Final fixes to ensure correct run of the program

--- bitonicsort.cu
+++ bitonicsort.cu
@@ -4,26 +4,51 @@
 #include <cuda_runtime.h>
 #define N 256
 
+__device__ int power(int base, int exp) {
+    int power = 1;
+    for (int i = 0; i < exp; i++)
+        power *= base;
+    return power;
+}
+
+__device__ int logOfBase2(int size) {
+    int log = 0;
+    while(size > 1) {
+        size /=2;
+        log++;
+    }
+    return log;
+}
+
 
 __global__ void bitonic_sort_device(int *d_data, int size) {
     int index = threadIdx.x;
 
-    for(int group_size = 2; group_size <= size; group_size *=2){
-        for(int swap_distance = group_size/2; swap_distance > 0; swap_distance /=2){
-            
-            if (index < size){
-                int swap_index = index ^ swap_distance;
+    int logof2 = logOfBase2(size);
 
-                if (swap_index > index){
-                    int direction = (index / group_size) % 2 == 0;
+    for (int i_step = 0; i_step < logof2; i_step++) {
+        for(int j_stage = 0; j_stage <= i_step; j_stage++) {
 
-                    if (((d_data[index] > d_data[swap_index]) && direction ==1) || ((d_data[index] < d_data[swap_index]) && direction ==0)){    
-                        int temp = d_data[index];
-                        d_data[index] = d_data[swap_index];
-                        d_data[swap_index] = temp;
+            if (index < size) {
+                if (index % power(2, i_step - j_stage + 1) < power(2, i_step - j_stage)){
+                    int length_sequence = power(2, i_step - j_stage + 1);
+                    int swap_index = index + (length_sequence/2);
+
+                    if (swap_index < size) {
+                        int direction = 0;
+                        if ((index / power(2, i_step + 1)) % 2 == 0)
+                            direction = 1;
+                        else
+                            direction = -1;
+                        
+                        if (((d_data[index] > d_data[swap_index]) && direction ==1) || ((d_data[index] < d_data[swap_index]) && direction ==-1)){
+                            int temp = d_data[index];
+                            d_data[index] = d_data[swap_index];
+                            d_data[swap_index] = temp;
+                        }
                     }
                 }
-            }    
+            }
         __syncthreads();
         }
     }
